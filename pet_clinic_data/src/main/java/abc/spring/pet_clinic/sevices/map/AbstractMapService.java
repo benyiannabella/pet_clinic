f8360a419de80bcf47ece833fee26ebb578e9b83
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2021-12-11 21:32:35 +0200

    implement and extend AbstractMapService

---
+++ pet_clinic_data/src/main/java/abc/spring/pet_clinic/sevices/map/AbstractMapService.java
@@ -0,0 +1,33 @@
+package abc.spring.pet_clinic.sevices.map;
+
+import javax.persistence.Id;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class AbstractMapService<T,ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+    Set<T> findAll(){
+        return new HashSet<>(map.values());
+    }
+
+    T findById(Long id){
+        return map.get(id);
+    }
+
+    T save(ID id, T object){
+        map.put(id, object);
+        return object;
+    }
+
+    void deleteById(Long id){
+        map.remove(id);
+    }
+
+    void delete(T object){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+}
