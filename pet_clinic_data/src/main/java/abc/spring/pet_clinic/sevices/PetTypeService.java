a42eee63ffa8d12ed9c3e25f5a984e8a097ff5fa
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2021-12-16 21:59:20 +0200

    cre

---
+++ pet_clinic_data/src/main/java/abc/spring/pet_clinic/sevices/PetTypeService.java
@@ -0,0 +1,9 @@
+package abc.spring.pet_clinic.sevices;
+
+import abc.spring.pet_clinic.model.PetType;
+
+public interface PetTypeService  extends CrudService<PetType,Long> {
+
+
+
+}
--- pet_clinic_data/src/main/java/abc/spring/pet_clinic/sevices/map/PetTypeMapService.java
+++ pet_clinic_data/src/main/java/abc/spring/pet_clinic/sevices/map/PetTypeMapService.java
@@ -1,16 +1,16 @@
 package abc.spring.pet_clinic.sevices.map;
 
-import abc.spring.pet_clinic.model.Pet;
-import abc.spring.pet_clinic.sevices.PetService;
+import abc.spring.pet_clinic.sevices.PetTypeService;
+import abc.spring.pet_clinic.model.PetType;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
 
     @Override
-    public Set<Pet> findAll() {
+    public Set<PetType> findAll() {
         return super.findAll();
     }
 
@@ -20,19 +20,17 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
     }
 
     @Override
-    public void delete(Pet object) {
+    public void delete(PetType object) {
         super.delete(object);
     }
 
     @Override
-    public Pet save(Pet object) {
+    public PetType save(PetType object) {
         return super.save(object);
     }
 
     @Override
-    public Pet findById(Long id) {
+    public PetType findById(Long id) {
         return super.findById(id);
     }
-
-
 }
