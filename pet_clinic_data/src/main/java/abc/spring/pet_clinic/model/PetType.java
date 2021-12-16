71645a353f530e53f76a055e229f7a1123b00133
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2021-12-16 21:30:27 +0200

    add Visit class

--- pet_clinic_data/src/main/java/abc/spring/pet_clinic/model/PetType.java
+++ pet_clinic_data/src/main/java/abc/spring/pet_clinic/model/PetType.java
@@ -2,5 +2,13 @@ package abc.spring.pet_clinic.model;
 
 public class PetType extends BaseEntity{
 
-    private String type;
+    private String typeName;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
 }
